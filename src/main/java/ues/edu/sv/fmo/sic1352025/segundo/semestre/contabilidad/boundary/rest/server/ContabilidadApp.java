b616dd3400f906ca0d7d72eccd71d98a6044a62c
Jordi Martínez <michaeljordimartinez@gmail.com>
2025-11-23 17:33:55 -0600

    añadido: path de aplicacion REST

---
+++ src/main/java/ues/edu/sv/fmo/sic1352025/segundo/semestre/contabilidad/boundary/rest/server/ContabilidadApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ues.edu.sv.fmo.sic1352025.segundo.semestre.contabilidad.boundary.rest.server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ * @author jordi
+ */
+@ApplicationPath("v1_1")
+public class ContabilidadApp extends Application{
+    // http://localhost:9080/contabilidad-0.1-SNAPSHOT/v1/
+
+}
